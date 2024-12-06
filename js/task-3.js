32bf9844ac1a313152b11b01192e2d1c10f93856
Viktor Romaniukov <viktorrvi@email.ua>
2024-12-06 17:52:37 +0200

    add hw

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,11 @@
-function getElementWidth(content, padding, border) {
-  const borderBox =
-    Number.parseFloat(content) +
-    Number.parseFloat(padding) * 2 +
-    Number.parseFloat(border) * 2;
-  return borderBox;
+function checkForSpam(message) {
+    return message.toLowerCase().includes("spam") || message.toLowerCase().includes("sale");
 }
 
-console.log(`Загальна ширина елемента складає: ${getElementWidth('50px', '8px', '4px')}px.`);
-console.log(`Загальна ширина елемента складає: ${getElementWidth('60px', '12px', '8.5px')}px.`);
-console.log(`Загальна ширина елемента складає: ${getElementWidth('200px', '0px', '0px')}px.`);
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
